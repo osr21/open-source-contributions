557e9f11cd4b8c4002cabbd27dddfc7cc633a93d
osr21 <tobimax241@gmail.com>
2026-05-09 07:30:24 +0100

    feat: add sleep/delay utility

---
+++ utils/sleep.js
@@ -0,0 +1,10 @@
+/**
+ * Pause execution for a given number of milliseconds.
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = { sleep };
