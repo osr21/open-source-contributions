eaade09ea5f4b59e12732ea2daa63b10edef7d5e
osr21 <tobimax241@gmail.com>
2026-05-09 07:42:30 +0100

    feat: add deep equality comparison utility

---
+++ utils/deep-equal.js
@@ -0,0 +1,15 @@
+function deepEqual(a, b) {
+  if (a === b) return true;
+  if (typeof a !== typeof b || a === null || b === null) return false;
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+    return a.every((v, i) => deepEqual(v, b[i]));
+  }
+  if (typeof a === 'object') {
+    const keysA = Object.keys(a), keysB = Object.keys(b);
+    if (keysA.length !== keysB.length) return false;
+    return keysA.every(k => deepEqual(a[k], b[k]));
+  }
+  return false;
+}
+module.exports = { deepEqual };
