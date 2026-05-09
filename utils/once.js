928d99805740d1531b7945526c356466a9bbf46f
osr21 <tobimax241@gmail.com>
2026-05-09 07:42:35 +0100

    feat: add once utility to call a function only one time

---
+++ utils/once.js
@@ -0,0 +1,8 @@
+function once(fn) {
+  let called = false, result;
+  return function (...args) {
+    if (!called) { called = true; result = fn.apply(this, args); }
+    return result;
+  };
+}
+module.exports = { once };
