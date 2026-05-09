8c47c39b2a4d74808db2755b7ede346969d974cd
osr21 <tobimax241@gmail.com>
2026-05-09 07:43:11 +0100

    feat: add noop and identity functional utilities

---
+++ utils/noop.js
@@ -0,0 +1,5 @@
+function noop() {}
+function identity(x) { return x; }
+function alwaysTrue() { return true; }
+function alwaysFalse() { return false; }
+module.exports = { noop, identity, alwaysTrue, alwaysFalse };
